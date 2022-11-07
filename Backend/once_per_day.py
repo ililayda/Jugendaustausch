4c778269fe4e61b31729b95cd0792cd17e88d8ff
TheOrangeMan96 <112876142+TheOrangeMan96@users.noreply.github.com>
2022-11-07 15:36:22 +0100

    Add files via upload

---
+++ Backend/once_per_day.py
@@ -0,0 +1,16 @@
+import schedule
+import time
+import Neue_Tabellen as nt
+import loeschung as l
+import Automatische_Loeschung as al
+
+def job():
+    al.main()
+    l.main()
+    nt.main()
+
+schedule.every().day.at("10:30").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
