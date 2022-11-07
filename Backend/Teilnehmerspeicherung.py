c3794e710df44973f1bea532ad52c3c3611871f4
TheOrangeMan96 <112876142+TheOrangeMan96@users.noreply.github.com>
2022-11-07 13:46:01 +0100

    Update Teilnehmerspeicherung.py

--- Backend/Teilnehmerspeicherung.py
+++ Backend/Teilnehmerspeicherung.py
@@ -38,7 +38,7 @@ def main():
     
     if conn is not None:
         with conn:
-            new_participant = (str(today),anrede ,name ,vorname ,klasse ,klassenleitung ,mobilfunknummer ,volljährig ,0)
+            new_participant = (str(dt.today),anrede ,name ,vorname ,klasse ,klassenleitung ,mobilfunknummer ,volljährig ,0)
             insert_participant(conn, new_participant)
     else:
         print("Error! cannot create the database connection.")
