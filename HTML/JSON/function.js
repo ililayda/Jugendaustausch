2bdfa9124718358304ebeebe7c631ae176fb72cb
Max679g12 <max.g12@gso.schule.koeln>
2022-11-08 15:45:08 +0100

    HTML

--- HTML/JSON/function.js
+++ HTML/JSON/function.js
@@ -1,6 +1,10 @@
-$('#inputpassword, #confirmpassword').on('keyup', function () {
-  if ($('#inputpassword').val() == $('#confirmpassword').val()) {
-    $('#pwmessage').html('Matching').css('color', 'green');
-  } else
-    $('#pwmessage').html('Not Matching').css('color', 'red');
-});
+$('#inputpassword, #confirmpassword').on('keyup', function ()
+    {
+    if ($('#inputpassword').val() == $('#confirmpassword').val())
+    {
+        $('#pwmessage').html('Matching').css('color', 'green');
+    }
+    else
+        $('#pwmessage').html('Not Matching').css('color', 'red');
+    }
+);
